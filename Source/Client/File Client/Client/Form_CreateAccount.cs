0eac714e65c6bafe237db8589063e3fdb058baf9
ThaiNhatThu <21522648@gm.uit.edu.vn>
2023-05-19 21:50:55 +0700

    up

---
+++ Source/Client/File Client/Client/Form_CreateAccount.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form_CreateAccount : Form
+    {
+
+        private const string serverIpAddress = "127.0.0.1";
+        private const int serverPort = 8080;
+        public Form_CreateAccount()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form_CreateAccount_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button_CreateAcc_Click(object sender, EventArgs e)
+        {
+            string username = textBox_CreateUser.Text;
+            string password = textBox_CreatePass.Text;
+            string confirmPassword = textBox_ConfirmPass.Text;
+            
+            //Kiểm tra thông tin nhập vào 
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(confirmPassword))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin.");
+                return;
+            }
+
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Mật khẩu và xác nhận mật khẩu không khớp.");
+                return;
+            }
+
+            try
+            {
+                using (TcpClient client = new TcpClient(serverIpAddress, serverPort))
+                {
+                    using (NetworkStream stream = client.GetStream())
+                    {
+                        byte[] usernameBytes = Encoding.ASCII.GetBytes(username);
+                        byte[] passwordBytes = Encoding.ASCII.GetBytes(password);
+
+                        // Gửi thông điệp Tên và Mật khẩu đến server
+                        stream.Write(usernameBytes, 0, usernameBytes.Length);
+                        stream.Write(passwordBytes, 0, passwordBytes.Length);
+
+                        // Nhận kết quả từ server, đã tạo thành công hay chưa 
+                        byte[] buffer = new byte[1024];
+                        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                        string result = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                        MessageBox.Show(result);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi tạo tài khoản: {ex.Message}");
+            }
+        }
+
+        private void checkBox_ShowPass_CheckedChanged(object sender, EventArgs e) //Nút hiện mật khẩu 
+        {
+            if (checkBox_ShowPass.Checked)
+            {
+                textBox_CreatePass.UseSystemPasswordChar = false;
+                textBox_ConfirmPass.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                textBox_CreatePass.UseSystemPasswordChar = true;
+                textBox_ConfirmPass.UseSystemPasswordChar = true;
+            }
+        }
+    }
+}
--- Source/Client/File Client/Client/Form_Login.Designer.cs
+++ Source/Client/File Client/Client/Form_Login.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Login));
+            textBox_Username = new TextBox();
+            textBox_Password = new TextBox();
+            button_Login = new Button();
+            button_ForgotPass = new Button();
+            button_CreateAcc = new Button();
+            checkBox_ShowPass = new CheckBox();
+            SuspendLayout();
+            // 
+            // textBox_Username
+            // 
+            textBox_Username.BorderStyle = BorderStyle.None;
+            textBox_Username.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox_Username.ForeColor = Color.FromArgb(255, 128, 128);
+            textBox_Username.Location = new Point(133, 192);
+            textBox_Username.Name = "textBox_Username";
+            textBox_Username.Size = new Size(212, 23);
+            textBox_Username.TabIndex = 0;
+            // 
+            // textBox_Password
+            // 
+            textBox_Password.BorderStyle = BorderStyle.None;
+            textBox_Password.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox_Password.Location = new Point(133, 270);
+            textBox_Password.Name = "textBox_Password";
+            textBox_Password.Size = new Size(212, 23);
+            textBox_Password.TabIndex = 1;
+            textBox_Password.UseSystemPasswordChar = true;
+            // 
+            // button_Login
+            // 
+            button_Login.BackColor = Color.Transparent;
+            button_Login.FlatAppearance.BorderSize = 0;
+            button_Login.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            button_Login.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button_Login.FlatStyle = FlatStyle.Flat;
+            button_Login.Location = new Point(169, 373);
+            button_Login.Name = "button_Login";
+            button_Login.Size = new Size(110, 29);
+            button_Login.TabIndex = 2;
+            button_Login.UseVisualStyleBackColor = false;
+            // 
+            // button_ForgotPass
+            // 
+            button_ForgotPass.BackColor = Color.Transparent;
+            button_ForgotPass.FlatAppearance.BorderSize = 0;
+            button_ForgotPass.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            button_ForgotPass.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button_ForgotPass.FlatStyle = FlatStyle.Flat;
+            button_ForgotPass.Location = new Point(234, 309);
+            button_ForgotPass.Name = "button_ForgotPass";
+            button_ForgotPass.Size = new Size(134, 20);
+            button_ForgotPass.TabIndex = 3;
+            button_ForgotPass.UseVisualStyleBackColor = false;
+            // 
+            // button_CreateAcc
+            // 
+            button_CreateAcc.BackColor = Color.Transparent;
+            button_CreateAcc.FlatAppearance.BorderSize = 0;
+            button_CreateAcc.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            button_CreateAcc.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button_CreateAcc.FlatStyle = FlatStyle.Flat;
+            button_CreateAcc.Location = new Point(159, 418);
+            button_CreateAcc.Name = "button_CreateAcc";
+            button_CreateAcc.Size = new Size(120, 27);
+            button_CreateAcc.TabIndex = 4;
+            button_CreateAcc.UseVisualStyleBackColor = false;
+            button_CreateAcc.Click += button_CreateAcc_Click;
+            // 
+            // checkBox_ShowPass
+            // 
+            checkBox_ShowPass.FlatStyle = FlatStyle.Flat;
+            checkBox_ShowPass.Location = new Point(338, 274);
+            checkBox_ShowPass.Margin = new Padding(5);
+            checkBox_ShowPass.Name = "checkBox_ShowPass";
+            checkBox_ShowPass.Size = new Size(18, 17);
+            checkBox_ShowPass.TabIndex = 6;
+            checkBox_ShowPass.UseVisualStyleBackColor = true;
+            checkBox_ShowPass.CheckedChanged += checkBox_ShowPass_CheckedChanged;
+            // 
+            // Form_Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(812, 482);
+            Controls.Add(checkBox_ShowPass);
+            Controls.Add(button_CreateAcc);
+            Controls.Add(button_ForgotPass);
+            Controls.Add(button_Login);
+            Controls.Add(textBox_Password);
+            Controls.Add(textBox_Username);
+            Name = "Form_Login";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox_Username;
+        private TextBox textBox_Password;
+        private Button button_Login;
+        private Button button_ForgotPass;
+        private Button button_CreateAcc;
+        private CheckBox checkBox_ShowPass;
     }
 }
