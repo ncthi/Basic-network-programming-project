71636058b2a2202fe2d4668baa203466812145be
NguyenCaoThi <caothiu2003@gmail.com>
2023-05-20 12:07:35 +0700

    add socket

--- Source/Server/File Server/Server/Program.cs
+++ Source/Server/File Server/Server/Program.cs
@@ -5,13 +5,13 @@ namespace Server
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
+        ///
         [STAThread]
         static void Main()
         {
-            SQL_server Database = new SQL_server();
-            Database.ConnectSqlServer();
-            SocketServer server =new  SocketServer(3000);
+            SocketServer server =new  SocketServer(8080);
             Thread threadServer = new Thread(server.Listen);
+            threadServer.Start();
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form_FileManager());
