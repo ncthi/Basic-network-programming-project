a6734787372c90941b21edc48bfa58dcb3394aeb
ThaiNhatThu <21522648@gm.uit.edu.vn>
2023-05-22 23:01:27 +0700

    ahuhu

--- Source/Server/File Server/Server/Email.cs
+++ Source/Server/File Server/Server/Email.cs
@@ -8,68 +8,69 @@ using System.Threading.Tasks;
 
 namespace Server
 {
-    internal class Email
-    {
-        public static string Address = "cloudkeeper4@gmail.com"; 
-        public static string Password = "abcde"; 
-        string htmlFilePath = @"C:\LTM\email.html"; //đường dẫn tới file html 
-        public void SendPasswordResetEmail(string userEmail, string htmlFilePath) 
+        internal class Email
         {
-            string newPassword = GenerateRandomPassword();
-            string subject = "Reset Password";
+            public static string Address = "cloudkeeper4@gmail.com"; 
+            public static string Password = "yzzatuxcjlxqonmy"; 
+            string htmlFilePath = @"C:\Users\Nhat Thu\OneDrive - Trường ĐH CNTT - University of Information Technology\UIT\2ND YEAR\LAP TRINH MANG\Đồ án\Email.html"; //đường dẫn tới file html 
+            public void SendPasswordResetEmail(string userEmail, string htmlFilePath) 
+            {
+                string newPassword = GenerateRandomPassword();
+                string subject = "Reset Password";
             
-            string htmlBody = ReadHtmlFile(htmlFilePath);
-            string body = GetPasswordResetEmailBody(htmlBody, newPassword);
+                string htmlBody = ReadHtmlFile(htmlFilePath);
+                string body = GetPasswordResetEmailBody(htmlBody, newPassword);
 
-            Send(userEmail, subject, body); //gửi email đến địa chỉ email của người nhận 
-        }
-
-        private string GenerateRandomPassword() //Tạo mật khẩu ngẫu nhiên dài 8 kí tự 
-        {
-            const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-            Random random = new Random();
-            return new string(Enumerable.Repeat(validChars, 8) // Mật khẩu có độ dài 8 ký tự
-                .Select(s => s[random.Next(s.Length)]).ToArray());
-        }
-
-        private string ReadHtmlFile(string filePath)
-        {
-            string htmlContent = string.Empty;
-            try
-            {
-                htmlContent = File.ReadAllText(filePath);
+                Send(userEmail, subject, body); //gửi email đến địa chỉ email của người nhận 
             }
-            catch (Exception ex)
+
+            private string GenerateRandomPassword() //Tạo mật khẩu ngẫu nhiên dài 8 kí tự 
             {
-                Console.WriteLine("Đã xảy ra lỗi khi đọc file HTML: " + ex.Message);
+                const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+                Random random = new Random();
+                return new string(Enumerable.Repeat(validChars, 8) // Mật khẩu có độ dài 8 ký tự
+                    .Select(s => s[random.Next(s.Length)]).ToArray());
             }
-            return htmlContent;
-        }
 
-        private string GetPasswordResetEmailBody(string htmlBody,string newPassword)  //Lấy nôị dung html đã code sẵn và thay thế mật khẩu 
-        {
-            string body = htmlBody.Replace("{{newPassword}}", newPassword);
-            return body;
-        }
-        private void Send(string userEmail, string subject, string body)
-        {
-            using (SmtpClient smtp = new SmtpClient())
+            private string ReadHtmlFile(string filePath)
             {
-                smtp.Host = "smtp.gmail.com";
-                smtp.Port = 587;
-                smtp.EnableSsl = true;
-                smtp.Credentials = new NetworkCredential(Address, Password); 
-
-                using (MailMessage mail = new MailMessage(Address, userEmail))
+                string htmlContent = string.Empty;
+                try
                 {
-                    mail.Subject = subject; 
-                    mail.Body = body;
-                    mail.IsBodyHtml = true;
+                    htmlContent = File.ReadAllText(filePath);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error when reading HTML file: " + ex.Message);
+                }
+                return htmlContent;
+            }
 
-                    smtp.Send(mail);
+            //Lấy nôị dung html đã code sẵn và thay thế mật khẩu 
+            private string GetPasswordResetEmailBody(string htmlBody,string newPassword) 
+            {
+                string body = htmlBody.Replace("{{newPassword}}", newPassword); 
+                return body;
+            }
+            private void Send(string userEmail, string subject, string body)
+            {
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    smtp.Credentials = new NetworkCredential(Address, Password); 
+
+                    using (MailMessage mail = new MailMessage(Address, userEmail))
+                    {
+                        mail.Subject = subject; 
+                        mail.Body = body;
+                        mail.IsBodyHtml = true;
+
+                        smtp.Send(mail);
+                    }
                 }
             }
-        }
 
+        }
     }
-}
